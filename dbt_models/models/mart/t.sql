9fb57ef9135911e921b9406c0c4444f57682b4cb
shubhm13 <smishra.shubhammishra@gmail.com>
2024-06-01 02:15:38 +0530

    refactor

--- dbt_models/models/mart/t.sql
+++
@@ -1,8 +0,0 @@
---select count(distinct account_id_hashed) from {{ref('account_created')}} -- 12000
-
---select count(distinct account_id_hashed) from {{ref('account_closed')}} -- 3909
-
-
---select count(distinct account_id_hashed) from {{ref('account_reopened')}} -- 7
-
-select count(distinct AccountID) from {{ref('dim_account')}} where CurrentStatus = 'Open'
