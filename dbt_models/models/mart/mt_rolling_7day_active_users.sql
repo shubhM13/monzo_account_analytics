a00fa05ee8a75a07cc57ecb8e55ae5ece26ba94c
shubhm13 <smishra.shubhammishra@gmail.com>
2024-06-01 02:29:26 +0530

    refactor

--- dbt_models/models/mart/mt_rolling_7day_active_users.sql
+++ dbt_models/models/mart/mt_rolling_7day_active_users.sql
@@ -13,7 +13,7 @@ with
 select
     b.*,
     c.open_accounts as cumulative_open_accounts,
-    (b.rolling_7day_transacting_users / open_accounts) * 100 as rolling_7d_active_users
+    (b.rolling_7day_transacting_users / c.open_accounts) * 100 as rolling_7d_active_users
 from rolling_7day_activity b
 inner join {{ ref("mt_daily_cum_active_accounts") }} c on b.date = c.date
 order by b.date asc
